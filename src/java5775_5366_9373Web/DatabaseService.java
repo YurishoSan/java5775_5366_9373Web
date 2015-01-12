b543e12e5d9b34568c6c5793e514942e37f8ac21
YurishoSan <yurisho@gmail.com>
2015-01-13 00:44:24 +0200

    create repo

---
+++ src/java5775_5366_9373Web/DatabaseService.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package java5775_5366_9373Web;
+
+/**
+ * @author Yurisho
+ *
+ */
+public class DatabaseService
+{
+
+}
---
+++ src/java5775_5366_9373Web/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Yurisho
+ *
+ */
+package java5775_5366_9373Web;
